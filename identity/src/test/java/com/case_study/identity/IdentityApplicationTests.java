f4e506b85e4dc9db73d3d330dc25681358abbe0c
Jomanah <114154027+JBinFaris@users.noreply.github.com>
2026-08-31 11:22:55 +0300

    [CASE_STUDY] generated app from spring intilizer

---
+++ identity/src/test/java/com/case_study/identity/IdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.case_study.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
